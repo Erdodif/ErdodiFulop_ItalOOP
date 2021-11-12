1516583d5e254febc6fdca646fab91e20aee479f
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-11-12 20:18:25 +0100

    Absztinens és AbstinenceViolationException megalkotása

---
+++ src/hu/petrik/szemely/AbstinenceViolationException.java
@@ -0,0 +1,8 @@
+package hu.petrik.szemely;
+
+public class AbstinenceViolationException extends RuntimeException{
+
+    public AbstinenceViolationException() {
+        System.out.println("Nem engedheti meg magának, hogy alkoholos italt fogyasszon!");
+    }
+}
--- src/hu/petrik/szemely/Absztinens.java
+++ src/hu/petrik/szemely/Absztinens.java
@@ -1,4 +1,19 @@
 package hu.petrik.szemely;
 
+import hu.petrik.receptek.*;
+
 public class Absztinens {
+
+    public Absztinens() {
+
+    }
+
+    public void elfogyaszt(Ital ital) {
+        if (!(ital instanceof Alkoholos)) {
+            System.out.println("Elfogyaszthatja ezt az italt, mert nem alkoholos!");
+        }
+        else {
+            throw new AbstinenceViolationException();
+        }
+    }
 }
